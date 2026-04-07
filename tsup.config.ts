c0833483e91dc7d4828c4e478543bc02a463dc6e
Rajender Joshi <2614954+crup@users.noreply.github.com>
2026-04-07 16:17:51 +0530

    build(build): minify package output

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  sourcemap: true,
+  minify: true,
+  sourcemap: false,
   clean: true,
   external: ['react'],
 });
